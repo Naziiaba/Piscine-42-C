868ca9db552bad33d31967f5cce9a66cf5cc650f
Naziia Travels <35944002+Naziiaba@users.noreply.github.com>
2019-08-28 21:49:09 -0700

    Add files via upload

---
+++ d11/ft_create_elem.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_create_elem.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: naidyral <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/21 14:25:10 by naidyral          #+#    #+#             */
+/*   Updated: 2019/08/21 14:28:53 by naidyral         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "ft_list.h"
+
+t_list		*ft_create_elem(void *data)
+{
+	t_list *list;
+
+	list = malloc(sizeof(t_list));
+	if (list)
+	{
+		list->data = data;
+		list->next = NULL;
+	}
+	return (list);
+}
